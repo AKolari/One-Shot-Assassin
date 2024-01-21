14ec0bc47fe9039c52fb96721d1d9b1d97a35b27
AKolari <90071560+AKolari@users.noreply.github.com>
2024-01-21 16:44:42 -0500

    Add singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T m_Instance; 
+   
+
+    public static T Instance
+    {
+        get
+        {
+            if (m_Instance == null)
+            {
+                m_Instance = FindObjectOfType<T>();
+                if (m_Instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name;
+                    m_Instance = obj.AddComponent<T>();
+                }
+            }
+            return m_Instance;
+        }
+    }
+
+    
+    public virtual void Awake()
+    {
+        if (m_Instance == null)
+        {
+            
+            m_Instance = this as T;
+
+            
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            
+            Destroy(gameObject);
+        }
+    }
+}
