b988b4b7097788cc7bcf0d5858440f3fc0885dbb
SavionW-Works <72318904+SavionW-Works@users.noreply.github.com>
2024-01-21 16:38:14 -0500

    Added Bullet and Ricochet Prefabs
    The bullet has a 2D physics material called "Bouncy" other prefabs can add a force with a SCRIPT of a similar name "BouncySurface".
    
    Most prefabs add a small force to the bullet with "BouncySurface" to keep it going. This is not 100% sure if this is necessary but can add a bit of excitement.
    
    The bullet has a preset direction with the "Bullet" script but can be changed later on.

---
+++ Assets/Scripts/BouncySurface.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncySurface : MonoBehaviour
+{
+     public float BounceStrength;
+
+     private void OnCollisionEnter2D(Collision2D collision) //When colliding with another object...
+     {
+
+          Bullet bullet = collision.gameObject.GetComponent<Bullet>(); //Stores the (ball) game object the surface collides with  
+          /*
+               -Ball is an instance of the Ball class in the Ball.cs script 
+               -collision.gameObject checks the gameObject you collided with
+           */
+
+          if (bullet != null) //If collided object is not null (a ball)
+          {
+               Vector2 normal = collision.GetContact(0).normal;
+               //The normal vector of a surface is the vector perpendicular to it 
+               //GetContact(0) is simply the first contact point 
+
+               bullet.AddBulletForce(-normal * this.BounceStrength);
+          }
+
+     }
+}
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+     public float speed = 200.0f;
+     public Rigidbody2D _rigidbody;
+     public float start_x = 0;
+     public float start_y = 1;
+
+
+
+     private void Awake()
+     {
+          _rigidbody = GetComponent<Rigidbody2D>();
+     }
+
+     // Start is called before the first frame update
+     void Start()
+     {
+          AddStartingForce();
+     }
+
+     private void AddStartingForce()
+     {
+          //Chooses a force to fire in (will depend on Player direction)
+          
+
+
+          //Puts starting x and y in a vector called direction
+          Vector2 direction = new Vector2(start_x, start_y);
+
+          //Applies direction and force to ball
+          _rigidbody.AddForce(direction * this.speed);
+     }
+
+     public void AddBulletForce(Vector2 force)
+     {
+          _rigidbody.AddForce(force);
+     }
+
+     // Update is called once per frame
+     void Update()
+    {
+        
+    }
+}
