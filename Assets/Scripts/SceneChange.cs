4b1bb6f503e32a8a332464866e88f6fe55604eb6
Steven V <stevenvelez1368@gmail.com>
2024-01-24 01:41:51 -0500

    Added Framework for Score and Phase changing. Put UI elements on the first level and will Update it for all levels.

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public string sceneName;
+        public void LoadScene()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
